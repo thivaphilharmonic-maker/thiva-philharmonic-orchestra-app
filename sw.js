f2dbc76791b72fb4b116515706fc097d78b538bb
thivaphilharmonic-maker <thivaphilharmonic@gmail.com>
2026-09-14 00:36:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'thiva-philharmonic-v8';
+const CACHE_VERSION = 'thiva-philharmonic-v9';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PDF_CACHE = `${CACHE_VERSION}-pdfs`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
